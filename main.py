e7912c7111d474c93a9e5fab2e612030f5d2878e
JakubWilczek98 <jakubwilczek98@gmail.com>
2022-01-06 15:08:39 +0100

    Style changes

--- main.py
+++ main.py
@@ -2,39 +2,49 @@ import serial
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.ion()
-fig = plt.figure()
-
-i = 0
-x = []
-y0 = []
-y1 = []
-y2 = []
-y3 = []
-
-i=0
-ser = serial.Serial(port='/dev/cu.usbmodem14101', baudrate=9600)
-ser.close()
-ser.open()
-
-while True:
-    data = ser.readline()
-    print(data.decode())
-    x.append(i)
-    y0.append(data.decode()[0:5])
-    y1.append(data.decode()[6:11])
-    y2.append(data.decode()[12:17])
-    y3.append(data.decode()[18:23])
+if __name__ == "__main__":
     
-    plt.scatter(i, float(data.decode()[0:5]),c='y')
-    plt.scatter(i, float(data.decode()[6:11]),c='g')
-    plt.scatter(i, float(data.decode()[12:17]),c='blue')
-    plt.scatter(i, float(data.decode()[18:23]),c='black')
+    plt.ion()
+    fig = plt.figure()
     
+    #Var
+    i = 0
+    x = []
+    y0 = []
+    y1 = []
+    y2 = []
+    y3 = []
     
-    i+=1
-    plt.show()
-    plt.pause(1)
-    np.savetxt("results.csv", np.column_stack((x, y0, y1, y2, y3)), delimiter=",", fmt='%s')
+    #data read from serial port
+    ser = serial.Serial(port='/dev/cu.usbmodem14101', baudrate=9600)
+    ser.close()
+    ser.open()
+    
+    while True:
+        
+        data = ser.readline()
+        print(data.decode())
+        
+        #time
+        x.append(i)
+        #Temperature in Celsius from serial port
+        y0.append(data.decode()[0:5])
+        y1.append(data.decode()[6:11])
+        y2.append(data.decode()[12:17])
+        y3.append(data.decode()[18:23])
+        
+        #Live chart
+        plt.scatter(i, float(data.decode()[0:5]),c='y')
+        plt.scatter(i, float(data.decode()[6:11]),c='g')
+        plt.scatter(i, float(data.decode()[12:17]),c='blue')
+        plt.scatter(i, float(data.decode()[18:23]),c='black')
+        
+        i+=1
+        plt.show()
+        #Pause time 1 sec (same in arduino control program)
+        plt.pause(1)
+        
+        #Saving results to csv file
+        np.savetxt("results.csv", np.column_stack((x, y0, y1, y2, y3)), delimiter=",", fmt='%s')
     
 
