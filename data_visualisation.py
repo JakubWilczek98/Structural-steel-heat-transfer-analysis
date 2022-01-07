333416a9a2ed21744e196ed0fe5988c58a88c528
JakubWilczek98 <jakubwilczek98@gmail.com>
2022-01-07 10:57:23 +0100

    Added max_values, statistic_values

--- data_visualisation.py
+++ data_visualisation.py
@@ -6,7 +6,8 @@ import numpy as np
 def visualisation(results,name,path): 
     plt.figure(1)
     plt.plot(results[:,0],results[:,1], 'y' , results[:,0], results[:,2], 'g' ,results[:,0],results[:,3], 'b' ,results[:,0],results[:,4], 'black')
-    plt.ylim(20,30)
+    plt.ylim(20,70) #Chart range
+    plt.xlim(30,180)
     plt.grid()
     plt.title(name)
     plt.xlabel('Time [s]')
@@ -21,16 +22,36 @@ def mean_velue(results):
     mean_2 = np.mean(results[:,2])
     mean_3 = np.mean(results[:,3])
     mean_4 = np.mean(results[:,4])
-    return mean_1, mean_2, mean_3, mean_4
+    values = [mean_1, mean_2, mean_3, mean_4]
+    values = np.round(values,2)
+    return values
 
+def max_velue(results):
+    max_1 = np.max(results[:,1])
+    max_2 = np.max(results[:,2])
+    max_3 = np.max(results[:,3])
+    max_4 = np.max(results[:,4])
+    values = [max_1, max_2, max_3, max_4]
+    return values
+
+def statistic_values(mean_value, max_value, path):
+    statistic_values = []
+    statistic_values.append(mean_value)
+    statistic_values.append(max_value)  
+    file_path = path.replace(".csv", "_stat.csv")
+    np.savetxt(file_path, np.column_stack((statistic_values)), delimiter=",", fmt='%s') 
+    
 if __name__ == "__main__":
 
-    file_path = 'Results/flat_bar_basic/state/basic_state.csv'
+    file_path = 'Results/flat_bar_basic/thermal/basic_1_thermal.csv'
     
     results = pd.read_csv(file_path, sep=',',header=None)
     results = pd.DataFrame(results).to_numpy()
+    results = results[30:180,:] #Range
     
-    visualisation(results,"Temperatura kalibracji", file_path)
+    visualisation(results,"Płaskonik podstawowy - pomiar 1", file_path)
     
-    print(mean_velue(results))
+    statistic_values(mean_velue(results), max_velue(results), file_path)
+    
+    
     
